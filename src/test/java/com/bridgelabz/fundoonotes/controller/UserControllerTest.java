604fb7d315e03792777e21dd721e6203c3a80869
kalyaniRane <kalyanirane19@gmail.com>
2020-09-11 13:14:56 +0530

    REFACTORED : UserController To Rectify The ServiceAutoConfiguration Issue

--- src/test/java/com/bridgelabz/fundoonotes/controller/UserControllerTest.java
+++ src/test/java/com/bridgelabz/fundoonotes/controller/UserControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -21,6 +22,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(UserController.class)
+@TestPropertySource(properties =
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration")
 public class UserControllerTest {
 
     @Autowired
