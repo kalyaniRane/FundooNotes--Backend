719e234eba8f4a3f9eca5e86887b57b236b19eb3
kalyaniRane <kalyanirane19@gmail.com>
2020-09-17 20:49:49 +0530

    ADDED : Config File For Interceptor

---
+++ src/main/java/com/bridgelabz/fundoonotes/interceptor/NoteServiceInterceptorAppConfig.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.fundoonotes.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class NoteServiceInterceptorAppConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    NoteServiceInterceptor noteServiceInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(noteServiceInterceptor).addPathPatterns("/note/**");
+    }
+}
