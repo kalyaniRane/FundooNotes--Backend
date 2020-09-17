af8c5fa1fba6b91562a5581d69053be30ebcd3aa
kalyaniRane <kalyanirane19@gmail.com>
2020-09-17 19:58:52 +0530

    ADDED : Interceptor Class For Implement Interceptor Concept

---
+++ src/main/java/com/bridgelabz/fundoonotes/interceptor/NoteServiceInterceptor.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.fundoonotes.interceptor;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class NoteServiceInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+}
